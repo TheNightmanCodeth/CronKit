ba361be326ad84d713cbed6dd3bb979a68f8a91b
Joseph Diragi <joseph.diragi@icloud.com>
2022-10-02 11:58:06 -0400

    Make sure to include current/past cron jobs when writing new ones to crontab

--- Sources/CronKit/Util.swift
+++ Sources/CronKit/Util.swift
@@ -23,29 +23,46 @@ public func addCronJob(entry: any Recurrable) throws -> String {
     let cronInput = Pipe()
     let cronOutput = Pipe()
     let cronWrite = Process()
+    let cronRead = Process()
+    let cronReadOut = Pipe()
+    cronRead.standardOutput = cronReadOut
+    cronRead.arguments = ["-l"]
+    cronRead.executableURL = URL(fileURLWithPath: "/usr/bin/crontab")
+    try cronRead.run()
+    cronRead.waitUntilExit()
+    print("Test")
+    
     cronWrite.standardOutput = cronOutput
     cronWrite.standardError = cronOutput
     cronWrite.arguments = ["-c", "crontab", "-"]
     cronWrite.executableURL = URL(fileURLWithPath: "/bin/sh")
     cronWrite.standardInput = cronInput
     
+    // Add previous jobs
+    cronInput.fileHandleForWriting.write(cronReadOut.fileHandleForReading.readDataToEndOfFile())
+    
     // Add newline to bottom of crontab
-    cronInput.fileHandleForWriting.write("\n".data(using: .utf8)!)
+    cronInput.fileHandleForWriting.write("\n\n".data(using: .utf8)!)
     
     // Label this entry
     let label = try entry.makeLabel()
     cronInput.fileHandleForWriting.write(label.data(using: .utf8)!)
     
-    // Linebreak
+    // new line
     cronInput.fileHandleForWriting.write("\n".data(using: .utf8)!)
     
     // Add this job to crontab under label comment
     cronInput.fileHandleForWriting.write(entry.toCronEntry().data(using: .utf8)!)
+    cronInput.fileHandleForWriting.write("\n".data(using: .utf8)!)
     cronInput.fileHandleForWriting.closeFile()
     
     try cronWrite.run()
     cronWrite.waitUntilExit()
     
     let outputData = cronOutput.fileHandleForReading.readDataToEndOfFile()
-    return (String(data: outputData, encoding: .utf8)!)
+    return (String(data: cronReadOut.fileHandleForReading.readDataToEndOfFile(), encoding: .utf8)!)
+}
+
+func findJob(_ id: String) -> Int {
+    return 0
 }
